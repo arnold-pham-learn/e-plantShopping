730dcd8586d7e0e690767b81b14f775bdb9cdfeb
Khuong Pham <khuongphampn@gmail.com>
2025-02-20 11:38:52 +0700

    Task 2: State management using Redux

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,27 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-
+      const newItem = action.payload;
+      const existItem = state.items.find(item => item.name === newItem.name);
+      if(existItem){
+        existItem.quantity++;
+      }else{
+        state.items.push({...newItem, quantity: 1});
+      }
+      console.log(state.items);
     },
     removeItem: (state, action) => {
+      const toRemoveItem = action.payload;
+      state.items = state.items.filter(item => item.name !== toRemoveItem.name);
     },
     updateQuantity: (state, action) => {
-
-
+      const {toUpdateItem, qty} = action.payload;
+      const existItem = state.items.find(item => item.name === toUpdateItem.name);
+      if(existItem){
+        existItem.quantity = qty;
+      }else{
+        state.items.push({...existItem, quantity: qty});
+      }
     },
   },
 });

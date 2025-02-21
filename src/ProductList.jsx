1403d679db3f87803d6a986671d8753c8cc9ea87
Khuong Pham <khuongphampn@gmail.com>
2025-02-21 15:43:31 +0700

    fixbug: count item in cart not total quantity in cart

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -237,12 +237,14 @@ function ProductList() {
         textDecoration: 'none',
     }
     // Calculate total cost based on quantity for an item
-    const getTotalQuantity = () => {
-        let totalQuantity = 0;
+    const getCountItem = () => {
+        let count = 0;
         cart.forEach((item) => {
-            totalQuantity += item.quantity;
+            if(item.quantity > 0){
+                count ++;
+            }
         })
-        return totalQuantity;
+        return count;
     };
     const handleAddToCart = (plant) => {
         dispatch(addItem(plant));
@@ -282,7 +284,7 @@ function ProductList() {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><p className='cart_quantity_count'>{getTotalQuantity()}</p><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><p className='cart_quantity_count'>{getCountItem()}</p><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
             </div>
             {!showCart ? (

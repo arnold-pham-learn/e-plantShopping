15bdb1645fe6d03915fc82d61bf12b36108dba52
Khuong Pham <khuongphampn@gmail.com>
2025-02-21 15:18:46 +0700

    Task 4: invisible button Add to Cart and compute total quantity

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addItem } from './CartSlice';
 function ProductList() {
+    const cart = useSelector(state => state.cart.items);
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
@@ -235,6 +236,14 @@ function ProductList() {
         fontSize: '30px',
         textDecoration: 'none',
     }
+    // Calculate total cost based on quantity for an item
+    const getTotalQuantity = () => {
+        let totalQuantity = 0;
+        cart.forEach((item) => {
+            totalQuantity += item.quantity;
+        })
+        return totalQuantity;
+    };
     const handleAddToCart = (plant) => {
         dispatch(addItem(plant));
         setAddedToCart((preState) => ({
@@ -273,7 +282,7 @@ function ProductList() {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><p className='cart_quantity_count'>{getTotalQuantity()}</p><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
             </div>
             {!showCart ? (
@@ -290,7 +299,7 @@ function ProductList() {
                                         <img className="product-image" src={plant.image} alt={plant.name} />
                                         <p className='product-price'>{plant.cost}</p>
                                         <p>{plant.description}</p>
-                                        <button className='product-button'
+                                        <button className={`product-button ${cart.find(item => item.name === plant.name && item.quantity > 0) ? 'added-to-cart': ''}`}
                                             onClick={() => handleAddToCart(plant)}
                                         >Add to Cart</button>
                                     </div>
